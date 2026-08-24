4b83245a324d9dcb874a162922e3eb5c3d8e54e0
Sascha Karnatz <sascha@blish.cloud>
2026-08-24 14:52:08 +0200

    Remove rubocop:disable comments
    This will remove follow warning during the standardrb run:
    
    lib/generators/solidus_static_content/install/install_generator.rb: Warning: Metrics/LineLength has the wrong namespace - replace it with Layout/LineLength

--- lib/generators/solidus_static_content/install/install_generator.rb
+++ lib/generators/solidus_static_content/install/install_generator.rb
@@ -9,7 +9,7 @@ module SolidusStaticContent
       class_option :frontend, type: :string, default: "classic"
 
       def add_stylesheets
-        inject_into_file "vendor/assets/stylesheets/spree/frontend/all.css", " *= require spree/frontend/solidus_static_content\n", before: %r{\*/}, verbose: true # rubocop:disable Metrics/LineLength
+        inject_into_file "vendor/assets/stylesheets/spree/frontend/all.css", " *= require spree/frontend/solidus_static_content\n", before: %r{\*/}, verbose: true
       end
 
       def add_migrations
@@ -42,11 +42,11 @@ module SolidusStaticContent
       end
 
       def run_migrations
-        run_migrations = options[:auto_run_migrations] || ["", "y", "Y"].include?(ask("Would you like to run the migrations now? [Y/n]")) # rubocop:disable Metrics/LineLength
+        run_migrations = options[:auto_run_migrations] || ["", "y", "Y"].include?(ask("Would you like to run the migrations now? [Y/n]"))
         if run_migrations
           run "bin/rails db:migrate"
         else
-          puts "Skipping bin/rails db:migrate, don't forget to run it!" # rubocop:disable Rails/Output
+          puts "Skipping bin/rails db:migrate, don't forget to run it!"
         end
       end
     end
